7b2610bed720ab139bdb9045d611f49860436a68
michaelrains <floridamiker@yahoo.com>
2015-05-18 00:02:10 -0400

    Tweaked plotimage.php to optimize the logic.  When drawing the tic marks, only need to set $plot_y once per iteration of the outer for($y....) loop.  And again for the X tic marks loop.
    Added comments to plotimage.php.
    
    Following are comments for my previous check-in, but I forgot to add it in time.
    Added uploadfile.php which contains a copy of the bobdemo class
    and has conditional logic to insert ed uploaded file into MySQL if Server Method is 'POST',
    and returns an HTML form to provide an <input type="file"/> form to post to uploadfile.php

--- plotimage.php
+++ plotimage.php
@@ -14,11 +14,13 @@ for($i=0;$i<500;$i++) {
  }
   */
 
+//draw the cartesian axis.  First the Y-axis
 for ($y=-150; $y<150; $y++) {
     $plot_y = 250 + $y;
     $plot_x = 250 + 0;
     imagesetpixel($img, $plot_x, $plot_y, $ink);
 }
+//then the X-axis
 for ($x=-150; $x<150; $x++) {
     $plot_x = 250 + $x;
     $plot_y = 250 + 0;
@@ -26,25 +28,30 @@ for ($x=-150; $x<150; $x++) {
 }
 
 
-
-
-
+//now draw the circle with radius 50 pixels
+//increment by 0.05 so that in the severe angles, none of the Y values are skipped.
 for ($x=0; $x<50; $x=$x + 0.05) {
 
   $y = sqrt(2500 - $x * $x);  //based on the formula for a circle, is that r^2 = x^2 + y@ ?
   if ($y>500) die('bad math!!!');
 
-  $plot_x = 250 + $x;
+  //translate x and y into screen pixel locations
+  $plot_x = 250 + $x;  
   $plot_x2 = 250 - $x;
   $plot_y = 250 - $y;
   $plot_y2 = 250 + $y;  //the other side of the circle
 
+  //set the pixels.
+  //$x is incrementing by 0.05, and plot_y is a square root with decimals,
+  ////so I guess imagesetpixel is converting plot_x and plot_y 
+  //// to integers, truncating off the decimal portion
   imagesetpixel($img, $plot_x, $plot_y, $ink);
   imagesetpixel($img, $plot_x, $plot_y2, $ink);
   imagesetpixel($img, $plot_x2, $plot_y, $ink);
   imagesetpixel($img, $plot_x2, $plot_y2, $ink);
 }
 
+//now draw the tic marks for the Y-axis
 for ($y=-150; $y<=150; $y= $y+50) {
     $plot_y = 250 + $y;
     for ($x=-10; $x<=10; $x++) {
@@ -52,6 +59,7 @@ for ($y=-150; $y<=150; $y= $y+50) {
         imagesetpixel($img, $plot_x, $plot_y, $redink);
     }
 }
+//tic marks for the X-axis
 for ($x=-150; $x<=150; $x= $x+50) {
     $plot_x = 250 + $x;
     for ($y=-10; $y<=10; $y++) {
